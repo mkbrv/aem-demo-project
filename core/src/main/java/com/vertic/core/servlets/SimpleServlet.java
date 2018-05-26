03d103517652bba4b2152cc04649da1895b0afcf
Miklos Csere <miki@softwareengineer.ro>
2018-05-26 20:56:44 +0200

    Initial commit

---
+++ core/src/main/java/com/vertic/core/servlets/SimpleServlet.java
@@ -0,0 +1,41 @@
+package com.vertic.core.servlets;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.servlets.HttpConstants;
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.apache.sling.api.resource.ValueMap;
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Component;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+/**
+ * Servlet that writes some sample content into the response. It is mounted for
+ * all resources of a specific Sling resource type. The
+ * {@link SlingSafeMethodsServlet} shall be used for HTTP methods that are
+ * idempotent. For write operations use the {@link SlingAllMethodsServlet}.
+ */
+@Component(service=Servlet.class,
+           property={
+                   Constants.SERVICE_DESCRIPTION + "=Simple Demo Servlet",
+                   "sling.servlet.methods=" + HttpConstants.METHOD_GET,
+                   "sling.servlet.resourceTypes="+ "demo-project/components/structure/page",
+                   "sling.servlet.extensions=" + "txt"
+           })
+public class SimpleServlet extends SlingSafeMethodsServlet {
+
+    private static final long serialVersionUid = 1L;
+
+    @Override
+    protected void doGet(final SlingHttpServletRequest req,
+            final SlingHttpServletResponse resp) throws ServletException, IOException {
+        final Resource resource = req.getResource();
+        resp.setContentType("text/plain");
+        resp.getWriter().write("Title = " + resource.adaptTo(ValueMap.class).get("jcr:title"));
+    }
+}
